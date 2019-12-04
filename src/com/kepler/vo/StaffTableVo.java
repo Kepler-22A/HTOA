bf3377ffdc91101118022c855b49cb5f36647d8e
Tom <1642558447@qq.com>
2019-12-04 15:07:25 +0800

    梁提交的empdatapage

--- src/com/kepler/vo/StaffTableVo.java
+++ src/com/kepler/vo/StaffTableVo.java
@@ -4,8 +4,8 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "staff")
-public class staffVo {
+@Table(name = "StaffTable")
+public class StaffTableVo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  String staffId ;
