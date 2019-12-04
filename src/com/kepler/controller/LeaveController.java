1e9406d1bf4df5db2b77d0f016914214a7824be2
ZiTao-GitHub <1784657658@qq.com>
2019-12-04 20:10:03 +0800

    梁提交

---
+++ src/com/kepler/controller/LeaveController.java
@@ -0,0 +1,7 @@
+package com.kepler.controller;
+
+/**
+ * Created by ASUS on 2019/12/4.
+ */
+public class LeaveController {
+}
---
+++ src/com/kepler/controller/StundetContorller.java
@@ -0,0 +1,12 @@
+package com.kepler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ASUS on 2019/12/4.
+ */
+@Controller
+@RequestMapping("student")
+public class StundetContorller {
+}
